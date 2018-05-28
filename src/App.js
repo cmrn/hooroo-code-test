7253963c26aec6e21e1936b4b31c47d12f8f6cd1
Cameron Moon <cameron@cameronmoon.com>
2018-05-28 12:02:34 +1000

    Remove create-react-app junk, add logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './qantas-logo.png';
+import styles from './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <img src={logo} className="App-logo" alt="Qantas" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
