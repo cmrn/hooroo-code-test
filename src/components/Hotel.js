1a2d9e2f2189b64febcc5b82c47ad7147ea662dd
Cameron Moon <cameron@cameronmoon.com>
2018-05-28 23:18:00 +1000

    Move hotel to own file

---
+++ src/components/Hotel.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Hotel.css';
+import Rating from './Rating';
+
+const Hotel = ({ hotel }) => {
+  return (
+    <div className="Hotel">
+      <div className="Hotel-image">
+        <span className="Hotel-promotion">{hotel.promotion}</span>
+        <img src={hotel.image} alt='' />
+      </div>
+      <div className="Hotel-details">
+        <div className="Hotel-title">
+          <h3>{hotel.title}</h3>
+          <Rating className="Hotel-rating" rating={hotel.rating} type={hotel.ratingType} />
+        </div>
+        <div className="Hotel-address">{hotel.address}</div>
+        <div className="Hotel-room">{hotel.roomName}</div>
+      </div>
+      { hotel.freeCancellation ? <div className="Hotel-cancellation">Free cancellation</div> : null }
+      <div className="Hotel-price">
+        <div className="Hotel-priceDescription">1 night total (AUD)</div>
+        <div>
+          <span className="Hotel-currencySymbol">$</span> <span className="Hotel-priceNumber">{hotel.price}</span>
+        </div>
+        { hotel.savings > 0 ? <div className="Hotel-savings">Save ${hotel.savings}~</div> : null }
+      </div>
+    </div>
+  )
+};
+
+export default Hotel;
